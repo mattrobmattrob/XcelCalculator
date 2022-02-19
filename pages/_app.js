4ab89743c129f96f0495285520259a2557407f44
Andrew Robinson <sirarkimedes@gmail.com>
2022-02-19 14:06:24 -0700

    Add title

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return <>
       <Head>
         <title>
-          ?
+          Xcel Energy TOU Calculator
         </title>
       </Head>
       <Component {...pageProps} />
